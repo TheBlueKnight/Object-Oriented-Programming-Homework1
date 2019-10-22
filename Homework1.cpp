0cbd96ef6c66322eb9acd18d466ed8dae4b05ae1
TheBlueKnight <34779247+TheBlueKnight@users.noreply.github.com>
2019-10-22 21:34:53 +0300

    Add files via upload

--- Homework1.cpp
+++ Homework1.cpp
@@ -1,62 +1,91 @@
- #include <iostream>
+//Student Name:		Furkan KUMSUZ 
+//Student ID :		040150152
+#include <iostream>
+
 using namespace std;
-int** generateMatrix(int rows, int cols){
-	int** temp =new int *[rows];
-	for(int i =0;i<rows;++i) {
-		temp[i]=new int[cols];
+const int MAX=10;													//Declaring constatnt size of array
+		 
+
+class Matrix{
+	
+	public:
+		int rows;
+		int cols;
+		int array[MAX][MAX];  										//Declaring two dimensional array 
+	
+		Matrix(){};  							          			//Declaring default constractor that does nothing
+		Matrix(int array[MAX][MAX],int rows,int cols);				//Prototype of parametrized constructor
+
+			
+		void print();
+};
+
+void Matrix :: print(){  											//Print Function
+    for(int i = 0; i < 3; ++i){
+	
+        for(int j = 0; j < 4; ++j)
+        {
+            cout <<array [i][j] << "  ";
+            if(j == 4 - 1)
+                cout << endl;
+    	}
+    }
+    
+}
+
+
+Matrix::Matrix(int data[MAX][MAX],int ro,int co){						//Parametrized Constructor
+			rows=ro;
+			cols=co;
+			for (int i=0;i<ro;i++){
+		
+				for (int j=0;j<co;j++){
+			
+					array[i][j]=data[i][j];
+			
+			}
+		
+		}
+	};
+	
+Matrix operator+ (const Matrix & M1, const Matrix & M2){  				//nON Member Overload Operator
+		
+	Matrix Total;
+	
+	for (int i=0;i<3;i++){
+		
+		for (int j=0;j<4;j++){
+			
+			Total.array[i][j]=M1.array[i][j]+M2.array[i][j];
+			
+		}
 		
 	}
-	return temp;
+	
+	return Total;
 	
 }
-void print(int** matrix, int rows, int cols){
-	for (int i=0;i<rows;++i){
-		for (int j=0;j<cols;++j){
-			cout<< matrix[i][j]<<" ";
-		}
-		cout << endl;
-	}
-	
-}
-void populateMatrix(int** matrix,int rows,int cols,int *src,int src_size){
-	if(rows * cols != src_size){
-		cout<<"Size of matrix is not equal size of source array!"<<std::endl;
-		exit(-1);		
-	}
-	int pos=0;
-	for (int i =0;i<rows;++i){
-		for(int j=0;j<cols;++j){
-			matrix[i][j]=src[pos++];
-		}
-	}
-}
-int** addMatrices(int** matA,int**matB,int rows,int cols){
-	int**resultMat=generateMatrix(rows,cols);
-	for(int i=0;i<rows;++i) {
-		for(int j=0;j<cols;++j) {
-			resultMat[i][j]=matA[i][j]+matB[i][j];	
-		}
-	}
-	return resultMat;
-}
+
+
 int main(){
-	int m1_src[]={1,2,3,4,5,6,7,8,9,10,11,12};
-	int m1_rows=3,m1_cols=4;
-	int** m1 = generateMatrix(m1_rows,m1_cols);
-	populateMatrix(m1,m1_rows,m1_cols,m1_src,12);
+	
+	int A[MAX][MAX]={ {1,2,3,4}, {5,6,7,8}, {9,10,11,12}};
+	int B[MAX][MAX]{ {10,20,30,40}, {50,60,70,80},{90,100,110,120}};
+	Matrix Mat1(A,3,4);
+	Matrix Mat2(B,3,4);
+	Matrix Total;
 	
 	
-	int m2_src[]={10,20,30,40,50,60,70,80,90,100,110,120};
-	int m2_rows=3,m2_cols=4;
-	int** m2 = generateMatrix(m2_rows,m2_cols);
-	populateMatrix(m2,m2_rows,m2_cols,m2_src,12);
+
+	
+	Total=Mat1+Mat2;
+	
+	cout<<"Total = Mat1 + Mat2\n\n";
+
+	Total.print();
 	
 	
-	print(m1,m1_rows,m1_cols);
-	cout<<endl;
-	print(m2,m2_rows,m2_cols);
-	cout<<endl;
-	print(addMatrices(m1,m2,m1_rows,m1_cols),m1_rows,m1_cols);
-   return 0;
+	
+	return 0;
+	
 }
- 
